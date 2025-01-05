c51b1e34a838a096ddecff3a7b65fddbe9a25770
Nitish Bhushan <nitishbhushan2013@gmail.com>
2025-01-05 16:36:14 +1100

    challenge 01: simple prompt

--- openai-assignments/src/index.js
+++ openai-assignments/src/index.js
@@ -19,9 +19,21 @@ const messages = [
     },
     {
         role: "user",
-        content: "what is my name ?", // check openai memory 
+        content: "explain Quantum Computing in brief", 
     },
 ]
+
+/**
+ * Challenge:
+ * 1. Ask OpenAI to explain something complicated 
+ *    to you. For example Quantum Computing.
+ * 
+ * Prompt Engineering Stretch Goals
+ * - See if you can control the level of complexity of 
+ *   the generated content, for example is this for 
+ *   10-year-olds or college kids?
+ * - See if you can control the length of the output.
+ * **/ 
 
 async function getCompletion(){
     try{
